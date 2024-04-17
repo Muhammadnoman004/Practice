68e771a7a3727c96b1edfd0eb4a3d69c0824d6f8
Noman Shamim <nomanshamim2004@gmail.com>
2024-04-17 21:58:24 +0500

    UseRef done...

--- src/Hooks/UseRef/UseReference.jsx
+++ src/Hooks/UseRef/UseReference.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useRef } from 'react';
 
 export default function UseReference() {
+
+  const InputRef = useRef('')
+  console.log(InputRef);
+
+  const [name, setname] = useState("Noman");
+
+  // useEffect(() => {
+  //   InputRef.current = InputRef.current + 1
+  // })
+
+  const handleChange = () => {
+    InputRef.current.style.backgroundColor = "grey"
+    InputRef.current.style.color = "white"
+    InputRef.current.focus()
+  }
+
   return (
     <div>
       <h1>UseRef Practice...</h1>
-      <input type="text" />
-      <button>reset</button>
+      <input type="text" ref={InputRef} value={name} onChange={(e) => setname(e.target.value)} />
+      <button onClick={() => setname('')}>reset</button>
+      <button onClick={handleChange}>Color Change</button>
+      {/* <h2>Current counting is {InputRef.current}</h2> */}
+
     </div>
   )
 }
