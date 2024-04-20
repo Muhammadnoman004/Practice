2dd4c39fee4ef0880e3a51882ee1ffbbcd2d9f36
Noman Shamim <nomanshamim2004@gmail.com>
2024-04-20 16:33:05 +0500

    Practice 's UseMamo,,

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import './App.css';
 import UseReference from './Hooks/UseRef/UseReference';
 import UseLayoutEffect from './Hooks/UseLayoutEffect/UseLayoutEffect';
 import ImperativeParent from './Hooks/UseImperativeHandle/ImperativeParent';
+import UseMamo from './Hooks/UseMamo/UseMamo';
 
 function App() {
   return (
     <div className="App">
-      <ImperativeParent />
+      <UseMamo />
     </div>
   );
 }
