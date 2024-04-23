b436529195f87a01800127280d8a555c273e3637
Noman Shamim <nomanshamim2004@gmail.com>
2024-04-23 23:44:31 +0500

    UseDeferredValue done...

--- src/Hooks/UseDeferredValue/UseDeferred.jsx
+++ src/Hooks/UseDeferredValue/UseDeferred.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { memo, useDeferredValue, useEffect, useState } from 'react'
 
 export default function UseDeferred() {
+
+  const [value, setvalue] = useState("");
+  const defferredValue = useDeferredValue(value)
+
+  const handleChange = (e) => {
+    const val = e.target.value
+    setvalue(val)
+  }
+
+  useEffect(() => {
+    console.log("value is", value);
+  }, [value])
+
+  useEffect(() => {
+    console.log("defferredValue is", defferredValue);
+  }, [defferredValue])
+
   return (
     <div>
       <h1>UseDeferredValue Practice...</h1>
+      <input type="text" onChange={handleChange} id="" />
+
+      {[...new Array(20)].map((_, i) => {
+        return <MemoizSearchText key={i} Value={defferredValue} />
+      })
+      }
     </div>
   )
 }
+
+const SearchText = ({ Value }) => {
+  let i = 0;
+  while (i < 10000000) {
+    i++
+  }
+  return <p>Search term : {Value}</p>
+}
+
+const MemoizSearchText = memo(SearchText);
